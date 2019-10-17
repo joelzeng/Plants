0d541b616ce26e54f896828829109448b080d57d
Joel Zeng <joelzeng@hotmail.com>
2019-10-16 22:11:26 -0400

    plant component done

---
+++ TapMangoPlants/Context/ApiContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using TapMangoPlants.Model;
+
+namespace TapMangoPlants.Context
+{
+    public class ApiContext : DbContext
+    {
+        public ApiContext(DbContextOptions<ApiContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Plant> Plants { get; set; }
+    }
+}
